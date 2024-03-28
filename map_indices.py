6d4a96e3a5af4cbe27a9adb657f5bf07044a96f7
Michael Acosta <mdacosta@cpp.edu>
2024-03-28 12:42:35 -0700

    Mapping from points to bounding boxes complete

--- map_indices.py
+++ map_indices.py
@@ -9,6 +9,11 @@ def gps_to_pixels(latitude, longitude):
     y_pixel = int((max_latitude - latitude) * pixel_per_degree_lat)
     return x_pixel, y_pixel
 
+def pixels_to_gps(x_pixel, y_pixel):
+    longitude = (x_pixel / pixel_per_degree_lon) + min_longitude
+    latitude = max_latitude - (y_pixel / pixel_per_degree_lat)
+    return round(latitude, 6), round(longitude, 6)
+
 def dms_to_decimal(degrees, minutes, direction):
     decimal_degrees = float(degrees) + float(minutes) / 60
     if direction in ['S', 'W']:
@@ -24,11 +29,41 @@ def parse_gps_coordinates(gps_string):
 
     return dms_to_decimal(degrees, minutes, direction)
 
+def csv_to_pixels(df):
+    with open('gpsMapping.txt', 'a') as f:
+        # Convert the DataFrame to a CSV-formatted string
+        csv_string = df.to_csv(index=False)
+
+        # Split the CSV-formatted string into lines
+        csv_lines = csv_string.splitlines()
+
+        # Avoid the header lines
+        header_line = csv_lines.pop(0)
+
+        # Iterate through the lines
+        for line in csv_lines:
+            fields = line.split(',')
+            latitude = float(fields[0])
+            longitude = float(fields[1])
+
+            # Translate GPS coordinates to pixels
+            x_pixel, y_pixel = gps_to_pixels(latitude, longitude)
+            f.write(f"Latitude: {latitude}, Longitude: {longitude}, Pixels: {x_pixel}, {y_pixel}\n")
+
 folder_path = './index_images'
 files = os.listdir(folder_path)
 
-csv_path = 'scout data farm vineyard august 2023.csv'
+csv_path = 'rounded scout data farm vineyard 2023.csv'
 df = pd.read_csv(csv_path)
+# df = df.round(6)
+# df.to_csv('rounded scout data farm vineyard 2023.csv', index=False)
+
+dictionary_mapping = {}
+
+with open('boundingBoxMapping.json', 'r') as file:
+    dictionary_mapping = json.load(file)
+
+dictionary_mapping = {tuple(eval(k)): v for k, v in dictionary_mapping.items()}
 
 with open('gpsMonitor.json') as f:
     monitorData = json.load(f)
@@ -60,33 +95,45 @@ for file_name in files:
 
     pixel_per_degree_lat = image_height / lat_range
     pixel_per_degree_lon = image_width / lon_range
-    
-    # Convert the DataFrame to a CSV-formatted string
-    csv_string = df.to_csv(index=False)
 
-    # Split the CSV-formatted string into lines
-    csv_lines = csv_string.splitlines()
+    averages = []
 
-    # Avoid the header lines
-    header_line = csv_lines.pop(0)
+    # Iterate through bounding boxes to find average value
+    for key, value in dictionary_mapping.items():
+        x_pixels, y_pixels = key
+        latitude, longitude = pixels_to_gps(x_pixels, y_pixels)
+        row_matches = df[(df['Latitude'] == latitude) & (df['Longitude'] == longitude)]
 
-    # Iterate through the lines
-    for line in csv_lines:
-        fields = line.split(',')
-        latitude = float(fields[0])
-        longitude = float(fields[1])
+        x_min = value[1]
+        y_min = value[2]
+        width = value[3]
+        height = value[4]
 
-        # Translate GPS coordinates to pixels
-        x_pixel, y_pixel = gps_to_pixels(latitude, longitude)
+        pixel_values = []
 
-        if (x_pixel < image_width and y_pixel < image_height):
-            r, g, b = heatmap_image.getpixel((x_pixel, y_pixel))
-            rgb_value = (r, g, b)
-            scale = 2 * g / 255 - 1
-            new_column_data.append(scale)
-        else:
-            new_column_data.append(-2)
-    
-    df[extracted_string] = new_column_data
+        # Define the percentage of the inner region to consider (k percent)
+        k_percent = 1
+
+        # Calculate the size of the inner square based on the given percentage
+        inner_width = int(width * k_percent)
+        inner_height = int(height * k_percent)
+
+        # Calculate the starting point for the inner square
+        inner_x_start = int(x_min + (width - inner_width) / 2)
+        inner_y_start = int(y_min + (height - inner_height) / 2)
+
+        # Loop over the inner region of the square
+        for x in range(inner_x_start, inner_x_start + inner_width):
+            for y in range(inner_y_start, inner_y_start + inner_height):
+                r, g, b = heatmap_image.getpixel((x, y))
+                rgb_value = (r, g, b)
+                scale = 2 * g / 255 - 1
+                pixel_values.append(scale)
+
+        # Take the average of the top k values
+        average = sum(pixel_values) / len(pixel_values)
+        for index, row in row_matches.iterrows():
+            df.at[index, extracted_string] = average
+        averages.append(average)
 
 df.to_csv(csv_path, index=False)
